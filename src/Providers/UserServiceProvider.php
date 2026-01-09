dd352e2ce4890f3c76605344a1addcdfc96f94af
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-01-09 10:00:12 +0100

    init

---
+++ src/Providers/UserServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Molitor\Article\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
