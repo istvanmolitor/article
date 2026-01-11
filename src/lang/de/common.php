41e5d0be0103c95ff73a86cf7c9ca2f9f63a4217
Molitor István <istvan.molitor@centralmediacsoport.hu>
2026-01-11 15:24:06 +0100

    Translation

---
+++ src/lang/de/common.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'article_group_navigation_label' => 'Artikelgruppen',
+    'article_group_plural_label' => 'Artikelgruppen',
+    'article_group_model_label' => 'Artikelgruppe',
+    'article_group_field_name' => 'Name',
+
+    'article_navigation_label' => 'Artikel',
+    'article_plural_label' => 'Artikel',
+    'article_model_label' => 'Artikel',
+    'article_field_title' => 'Titel',
+    'article_field_content' => 'Inhalt',
+    'article_field_author' => 'Autor',
+    'article_field_article_groups' => 'Artikelgruppen',
+
+    'author_navigation_label' => 'Autoren',
+    'author_plural_label' => 'Autoren',
+    'author_model_label' => 'Autor',
+    'author_field_name' => 'Name',
+    'navigation_group' => 'Inhalt',
+];
+
---
+++ src/lang/en/common.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'article_group_navigation_label' => 'Article Groups',
+    'article_group_plural_label' => 'Article Groups',
+    'article_group_model_label' => 'Article Group',
+    'article_group_field_name' => 'Name',
+
+    'article_navigation_label' => 'Articles',
+    'article_plural_label' => 'Articles',
+    'article_model_label' => 'Article',
+    'article_field_title' => 'Title',
+    'article_field_content' => 'Content',
+    'article_field_author' => 'Author',
+    'article_field_article_groups' => 'Article Groups',
+
+    'author_navigation_label' => 'Authors',
+    'author_plural_label' => 'Authors',
+    'author_model_label' => 'Author',
+    'author_field_name' => 'Name',
+    'navigation_group' => 'Content',
+];
+
+
